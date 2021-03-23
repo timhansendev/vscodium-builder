40f8c571bc4fced10c41b6e1bb209fca17caf5e8
Tim Hansen <tihansen@redhat.com>
2021-03-23 18:19:52 -0400

    Add comment on source of adaption from vscodium build scripts.
    Signed-off-by: Tim Hansen <tihansen@redhat.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Adapted from vscodium's build scripts.
+
 # figure out latest tag by calling MS update API
 UPDATE_INFO=$(curl https://update.code.visualstudio.com/api/update/darwin/stable/lol)
 export LATEST_MS_COMMIT=$(echo $UPDATE_INFO | jq -r '.version')
@@ -9,7 +11,6 @@ echo "Got the latest MS tag: ${LATEST_MS_TAG}"
 git clone ${repo} --branch ${tag} --depth 1 vscodium
 
 cd vscodium/
-
 
 git clone https://github.com/Microsoft/vscode.git --branch $LATEST_MS_TAG --depth 1
 npm config set scripts-prepend-node-path true
@@ -22,7 +23,6 @@ cd vscode || exit
 
 yarn monaco-compile-check
 yarn valid-layers-check
-
 yarn gulp compile-build
 yarn gulp compile-extensions-build
 yarn gulp minify-vscode
